a047050c3dbd3b10a8150aae79db304e8032c70a
Abdullah Nadeem <hafiz82567@gmail.com>
2026-08-02 14:36:08 +0500

    Add variable and data types practice

---
+++ variable,data type .py
@@ -0,0 +1,37 @@
+a=123
+print(a)
+print('type of a is :',type(a))
+print('id of a is :',id(a))
+
+# id means the address of the variable in memory
+
+a="hello"
+print(a)
+print('type of a is :',type(a))
+print('id of a is :',id(a))
+print("\n")
+a=complex(2,3)
+print(a)
+print('type of a is :',type(a))
+
+d=None
+print(d)
+print('type of d is :',type(d))
+
+# list means that it is a collection of data 
+# which is ordered and changeable and allows duplicate members
+
+print("List")
+l=[1,2,3,4,5]
+print(l)
+# tuple means that it is a collection of data
+# which is ordered and unchangeable and allows duplicate members
+
+print("Tuple")
+t=(1,2,3,4,5,6)
+print(t)
+
+
+
+
+
